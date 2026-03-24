5b1d383f041bec68bda7e6b2dbfd20a120023511
PSujit29 <iamsujeet29@gmail.com>
2026-03-24 19:33:43 +0545

    main files edited all at once implementing auth

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import LandingPage from "./pages/landing/LandingPage";
+import RouterConfig from "./router/Router";
+
+export default function App() {
+  return (
+    <>
+    <RouterConfig/>
+    {/* <LandingPage /> */}
+    
+    </>
+  );
+}
