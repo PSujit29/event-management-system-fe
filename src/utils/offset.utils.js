931a02298b8eb4a95e9116cdcb7053693b1231ed
PSujit29 <iamsujeet29@gmail.com>
2026-04-03 08:17:40 +0545

    remove unused components and files to clean up the codebase

--- src/utils/offset.utils.js
+++
@@ -1,5 +0,0 @@
-export function applyDayOffset(startDateIso, startOffset = 0) {
-  const date = new Date(startDateIso);
-  date.setDate(date.getDate() + Number(startOffset || 0));
-  return date.toISOString();
-}
